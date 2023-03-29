e44a9cf8c18abb81b0a75aca1294ceb544c9ab36
Yuheng Su <gipsyh.icu@gmail.com>
2023-03-29 07:16:52 +0000

    update impl ops for DdNode

--- src/ddnode.rs
+++ src/ddnode.rs
@@ -26,6 +26,18 @@ impl Drop for DdNode {
     }
 }
 
+impl AsRef<DdNode> for DdNode {
+    fn as_ref(&self) -> &DdNode {
+        self
+    }
+}
+
+impl AsMut<DdNode> for DdNode {
+    fn as_mut(&mut self) -> &mut DdNode {
+        self
+    }
+}
+
 impl Debug for DdNode {
     fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         unsafe { Cudd_PrintDebug(self.cudd.inner.manager, self.node, 1, 9) };
@@ -70,68 +82,68 @@ impl Not for &DdNode {
     }
 }
 
-impl BitAnd for DdNode {
-    type Output = Self;
-
-    fn bitand(self, rhs: Self) -> Self::Output {
-        assert!(self.cudd.inner == rhs.cudd.inner);
-        Self::new(self.cudd.clone(), unsafe {
-            Cudd_bddAnd(self.cudd.inner.manager, self.node, rhs.node)
-        })
-    }
-}
-
-impl BitAnd for &DdNode {
+impl<T: AsRef<DdNode>> BitAnd<T> for DdNode {
     type Output = DdNode;
 
-    fn bitand(self, rhs: Self) -> Self::Output {
-        assert!(self.cudd.inner == rhs.cudd.inner);
+    fn bitand(self, rhs: T) -> Self::Output {
+        assert!(self.cudd.inner == rhs.as_ref().cudd.inner);
         DdNode::new(self.cudd.clone(), unsafe {
-            Cudd_bddAnd(self.cudd.inner.manager, self.node, rhs.node)
+            Cudd_bddAnd(self.cudd.inner.manager, self.node, rhs.as_ref().node)
         })
     }
 }
 
-impl BitOr for DdNode {
-    type Output = Self;
-
-    fn bitor(self, rhs: Self) -> Self::Output {
-        assert!(self.cudd.inner == rhs.cudd.inner);
-        Self::new(self.cudd.clone(), unsafe {
-            Cudd_bddOr(self.cudd.inner.manager, self.node, rhs.node)
-        })
-    }
-}
-
-impl BitOr for &DdNode {
+impl<T: AsRef<DdNode>> BitAnd<T> for &DdNode {
     type Output = DdNode;
 
-    fn bitor(self, rhs: Self) -> Self::Output {
-        assert!(self.cudd.inner == rhs.cudd.inner);
+    fn bitand(self, rhs: T) -> Self::Output {
+        assert!(self.cudd.inner == rhs.as_ref().cudd.inner);
         DdNode::new(self.cudd.clone(), unsafe {
-            Cudd_bddOr(self.cudd.inner.manager, self.node, rhs.node)
+            Cudd_bddAnd(self.cudd.inner.manager, self.node, rhs.as_ref().node)
         })
     }
 }
 
-impl BitXor for DdNode {
-    type Output = Self;
-
-    fn bitxor(self, rhs: Self) -> Self::Output {
-        assert!(self.cudd.inner == rhs.cudd.inner);
-        Self::new(self.cudd.clone(), unsafe {
-            Cudd_bddXor(self.cudd.inner.manager, self.node, rhs.node)
-        })
-    }
-}
-
-impl BitXor for &DdNode {
+impl<T: AsRef<DdNode>> BitOr<T> for DdNode {
     type Output = DdNode;
 
-    fn bitxor(self, rhs: Self) -> Self::Output {
-        assert!(self.cudd.inner == rhs.cudd.inner);
+    fn bitor(self, rhs: T) -> Self::Output {
+        assert!(self.cudd.inner == rhs.as_ref().cudd.inner);
         DdNode::new(self.cudd.clone(), unsafe {
-            Cudd_bddXor(self.cudd.inner.manager, self.node, rhs.node)
+            Cudd_bddOr(self.cudd.inner.manager, self.node, rhs.as_ref().node)
+        })
+    }
+}
+
+impl<T: AsRef<DdNode>> BitOr<T> for &DdNode {
+    type Output = DdNode;
+
+    fn bitor(self, rhs: T) -> Self::Output {
+        assert!(self.cudd.inner == rhs.as_ref().cudd.inner);
+        DdNode::new(self.cudd.clone(), unsafe {
+            Cudd_bddOr(self.cudd.inner.manager, self.node, rhs.as_ref().node)
+        })
+    }
+}
+
+impl<T: AsRef<DdNode>> BitXor<T> for DdNode {
+    type Output = DdNode;
+
+    fn bitxor(self, rhs: T) -> Self::Output {
+        assert!(self.cudd.inner == rhs.as_ref().cudd.inner);
+        DdNode::new(self.cudd.clone(), unsafe {
+            Cudd_bddXor(self.cudd.inner.manager, self.node, rhs.as_ref().node)
+        })
+    }
+}
+
+impl<T: AsRef<DdNode>> BitXor<T> for &DdNode {
+    type Output = DdNode;
+
+    fn bitxor(self, rhs: T) -> Self::Output {
+        assert!(self.cudd.inner == rhs.as_ref().cudd.inner);
+        DdNode::new(self.cudd.clone(), unsafe {
+            Cudd_bddXor(self.cudd.inner.manager, self.node, rhs.as_ref().node)
         })
     }
 }
